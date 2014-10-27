1ec8c9e32dbefa0b7f0bd23df20d62000cdcf0b9
Justin Anastos <justin.anastos@gmail.com>
2014-10-27 10:55:26 -0400

    Initial commit.

---
+++ staged_github_files.js
@@ -0,0 +1,54 @@
+/*jshint node: true */
+
+var _ = require('underscore');
+var _s = require('underscore.string');
+var exec = require('shelljs').exec;
+var fs = require('fs');
+
+var cachedFileList;
+
+module.exports = {
+    all: function() {
+        var list;
+        var response;
+
+        // Return cache if we have one
+        if (cachedFileList) {
+            return cachedFileList;
+        }
+
+        response = exec('git status --porcelain', {silent: true, async: false}).output;
+        list = _s.trim(response).split(/\n/);
+
+        // Staged files
+        list = _.filter(list, function(line) {
+            return line.match(/^A./);
+        });
+
+        // Remove prefixes
+        list = _.map(list, function(line) {
+            return line.substr(3);
+        });
+
+        // Expand paths
+        list = _.flatten(_.compact(_.map(list, function(name) {
+            if (name.match(/\/$/)) {
+                return fs.readdirSync(name);
+            } else {
+                return name;
+            }
+        })));
+
+        cachedFileList = list;
+
+        return list;
+    },
+
+    filter: function(suffix) {
+        suffix = _s.ltrim(suffix, '.');
+
+        return _.filter(module.exports.all(), function(name) {
+            return name.match(new RegExp('\\.' + suffix + '$'));
+        });
+    }
+};
